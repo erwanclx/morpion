83455c160d89edb4d67ee41c1e55c9451097b9dd
Erwan <erwanclou@gmail.com>
2021-07-27 18:25:18 +0200

    morpion fonctionnel + début de l'ia

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-let gameactivation = true;
+let gameactivation = false;
 let activeplayer = "X";
-let gamestate = ["","","","","","","","",""]
+let gamestate = ["","","","","","","","",""];
+
+// Fonctionnement de l'ordinateur
+
+let random;
+let emptyCell = "";
 
 // Conditions de victoire
 
-const victorycondition = [
+const victorytemplate = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -17,13 +22,16 @@ const victorycondition = [
 
 // Const score
 
-const win = () => 'Le joueur ${activeplayer} a gagné, félicitation !'
-const equaliy = () => "Personne n'a gagné, peut être la prochaine fois ?"
+const equaliy = "Personne n'a gagné, peut être la prochaine fois ?"
 
 
 
 function newGame() {
-    let gameactivation = true;
+    gameactivation = true;
+    activeplayer= "X";
+    gamestate = ["","","","","","","","",""];
+    document.querySelectorAll(".cases").forEach(cell => cell.innerHTML = "");
+    this.innerHTML = "Vider la grille ?"
 }
 
 document.querySelectorAll(".cases").forEach(cell => cell.addEventListener("click", clickCase))
@@ -40,8 +48,49 @@ function clickCase(){
 
     gamestate[caseIndex] = activeplayer
     this.innerHTML = activeplayer
+
+    victorycheck()
 }
 
 function victorycheck() {
-    let victoryround 
+    let victoryround = false
+
+    for(let victorycondition of victorytemplate){
+        let val1 = gamestate[victorycondition[0]]
+        let val2 = gamestate[victorycondition[1]]
+        let val3 = gamestate[victorycondition[2]]
+        
+        if(val1 === "" || val2 === "" || val3 === ""){
+            continue    
+        }
+        if(val1 === val2 && val2 === val3){
+                victoryround = true
+                break
+            }
+        }
+   
+    if(victoryround){
+        alert("Le joueur " + activeplayer + " a gagné, félicitation !")
+        console.log("Le joueur " + activeplayer + " a gagné, félicitation !")
+        gameactivation = false
+        return
+    } 
+
+    if(!gamestate.includes("")){
+        alert(equaliy)
+        gameactivation = false
+        return
+    }
+
+    activeplayer = activeplayer === "X" ? "O" : "X"
+}
+
+function computerRound(){
+    random = Math.round(Math.random() * (8 - 0) + 0);
+    console.log(random);
+
+    if(gamestate[caseIndex] = emptyCell){
+        this.innerHTML = activeplayer;
+    }
+
 }
